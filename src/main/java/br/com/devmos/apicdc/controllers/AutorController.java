b5963afcc1be42c555f5d67ba8ee071260282ec4
Milton Oliveira Souza <miltonrubro@gmail.com>
2020-03-21 23:01:16 -0300

    Criação de um novo autor

--- src/main/java/br/com/devmos/apicdc/controllers/AutorController.java
+++ src/main/java/br/com/devmos/apicdc/controllers/AutorController.java
@@ -1,18 +1,30 @@
 package br.com.devmos.apicdc.controllers;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.devmos.apicdc.model.Autor;
 import br.com.devmos.apicdc.model.NovoAutorForm;
 
 @RestController
 public class AutorController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@PostMapping(value="/api/autor")
-	public void novo(@Valid NovoAutorForm form) {
-		System.out.println("teste ");
+	@Transactional
+	public void novo(@Valid @RequestBody NovoAutorForm form) {
+		Autor autor = form.novoAutor();
+		
+		manager.persist(autor);
+		
 	}
 
 }
