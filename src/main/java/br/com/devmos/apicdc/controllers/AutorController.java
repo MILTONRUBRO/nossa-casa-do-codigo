9d19fb56563f9a3f34147ff2260216e3507cab1c
Milton Oliveira Souza <miltonrubro@gmail.com>
2020-03-23 01:12:02 -0300

    Criando validação de e-mail unico para autor

--- src/main/java/br/com/devmos/apicdc/controllers/AutorController.java
+++ src/main/java/br/com/devmos/apicdc/controllers/AutorController.java
@@ -1,30 +1,36 @@
 package br.com.devmos.apicdc.controllers;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devmos.apicdc.model.Autor;
 import br.com.devmos.apicdc.model.NovoAutorForm;
+import br.com.devmos.apicdc.repositories.AutorRepository;
+import br.com.devmos.apicdc.validator.SemEmailDuplicadoValidator;
 
 @RestController
 public class AutorController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private AutorRepository autorRepository;
+	
+	@InitBinder("novoAutorForm")
+	public void init(WebDataBinder dataBinder) {
+		dataBinder.addValidators(new SemEmailDuplicadoValidator(autorRepository));
+	}
 	
 	@PostMapping(value="/api/autor")
 	@Transactional
 	public void novo(@Valid @RequestBody NovoAutorForm form) {
 		Autor autor = form.novoAutor();
-		
-		manager.persist(autor);
-		
+		autorRepository.save(autor);
 	}
 
 }
--- src/main/java/br/com/devmos/apicdc/model/Autor.java
+++ src/main/java/br/com/devmos/apicdc/model/Autor.java
@@ -2,6 +2,7 @@ package br.com.devmos.apicdc.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -27,9 +28,15 @@ public class Autor {
 	
 	@NotBlank
 	@Email
+	@Column(unique = true)
 	private String email;
 	
 	private LocalDateTime instateCriacao;
+	
+	@Deprecated
+	public Autor() {
+		
+	}
 
 	public Autor(@NotBlank String nome, String descricao, String email) {
 		this.nome = nome;
